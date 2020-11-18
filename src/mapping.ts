c7c6e3b8cae89c39b23d986058b419a7abc28324
aodhgan <gleesonaodhgan@gmail.com>
2020-11-18 15:50:49 -0800

    manifest and abis, no mappings

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,31 +1,30 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts"
 import {
-  CHIGastoken,
-  Approval,
   Transfer
-} from "../generated/CHIGastoken/CHIGastoken"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/"
+import { TransferEntity } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
+export function handleTransfer(event: Transfer): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = TransferEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new TransferEntity(event.transaction.from.toHex())
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    //entity.count = BigInt.fromI32(0)
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  //entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
   entity.owner = event.params.owner
   entity.spender = event.params.spender
+  entity.amount = (event.params.value).toString()
 
   // Entities can be written to the store with `.save()`
   entity.save()
@@ -44,23 +43,26 @@ export function handleApproval(event: Approval): void {
   //
   // The following functions can then be called on this contract to access
   // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.computeAddress2(...)
-  // - contract.decimals(...)
-  // - contract.free(...)
-  // - contract.freeFrom(...)
-  // - contract.freeFromUpTo(...)
-  // - contract.freeUpTo(...)
-  // - contract.name(...)
-  // - contract.symbol(...)
-  // - contract.totalBurned(...)
-  // - contract.totalMinted(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+
 }
 
-export function handleTransfer(event: Transfer): void {}
+export function handleCreateERC721Controlled(call: ethereum.Call): void {
+
+  /*
+
+  function createERC721Controlled(
+    string memory name,
+    string memory symbol,
+    string memory baseURI
+  ) returns ERC721Controlled
+  */
+
+  // the returned address is the Lootbox address
+  // we need to check ALL ERC20, 721 and 1155 to/from fields against this address
+
+  // create new LootBoxEntity here? assign ID = address?
+
+}
+
+
+//# write 5 mappings
